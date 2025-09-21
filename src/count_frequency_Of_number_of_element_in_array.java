3bc8ac72f5bf8c4d10a67c9e541a6ff4ce95db3c
Sushant Kushwaha <shushantk2009@gmail.com>
2025-09-21 18:21:27 +0530

    count_frequency_Of_number_of_element_in_array

---
+++ src/count_frequency_Of_number_of_element_in_array.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class count_frequency_Of_number_of_element_in_array {
+    public static void main(String[] args) {
+
+
+        int arr[]={10,5,4,2,10,4,10,6,10,5};
+        Map<Integer,Integer> freqMap =new HashMap<>();
+        for( int num : arr){
+                freqMap.put(num,freqMap.getOrDefault(num,0)+1);
+        }
+        for(Map.Entry<Integer,Integer> entry : freqMap.entrySet()){
+
+            System.out.println(entry.getKey()+" "+ entry.getValue());
+        }
+
+
+    }
+}
